3ad49ed2ce677c9fd66e6e608f7cfa7ba7cea601
vmsou <viniciusmarcs@icloud.com>
2022-08-03 22:16:30 -0300

    add: Leitura de Arquivo de Texto

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+/* Trabalho 1 - Máquinas de Estados Finitos
+Aluno: Vinícius Marques da Silva de Oliveira
+Data: 10/08/2022
+*/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct TextData {
+    int size;
+    std::vector<std::string> texts;
+};
+
+std::ostream& operator<<(std::ostream& os, const TextData& td) {
+    os << td.size;
+    for (std::size_t i = 0; i < td.texts.size(); ++i) {
+        const std::string& text = td.texts[i];
+        os << text;
+        if (i < td.texts.size() - 1) os << '\n';
+    }
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, TextData& td) {
+    int size = 0;
+    is >> size;
+    td.size = size;
+    
+    
+    std::string text;
+    while (size >= 0 && std::getline(is, text)) { 
+        td.texts.push_back(text); 
+        --size;    
+    }
+    return is;
+}
+
+int main() {
+    std::cout << "> Start" << '\n';
+    std::ifstream ifs{"text.txt"};
+    if (!ifs) throw std::runtime_error{"Não foi possivel abrir o arquivo."};
+    
+    TextData td{};
+    ifs >> td;
+    std::cout << td << '\n';
+    std::cout << "> End" << '\n';
+}
