baa3c5109e05c8080592968ba85faf486984b1a1
vmsou <viniciusmarcs@icloud.com>
2022-08-05 16:11:41 -0300

    main: solicitação de arquivo por loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,66 @@
 /* Trabalho 1 - Máquinas de Estados Finitos
 Aluno: Vinícius Marques da Silva de Oliveira
 Data: 10/08/2022
+
+Regras:
+alfabeto = {a ,b, c}
+linguagem = {x | x ∈ {a, b}*} e cada a seguido por bb
 */
 
 #include <fstream>
 #include <iostream>
 
+#include "parser.hpp"
 #include "reading.hpp"
 #include "tokenizer.hpp"
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector);
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &vector);
 
 int main() {
-    std::cout << "> Start" << '\n';
-    std::ifstream ifs{"text.txt"};
-    if (!ifs) throw std::runtime_error{"Não foi possivel abrir o arquivo."};
-    
-    TextData td{};
-    ifs >> td;
-    
+    std::cout << "> Inicio: Maquina de Estado Finito" << '\n';
+    std::cout << "Arquivos: 'data/text.txt', 'data/valido.txt', 'data/invalido.txt'\n";
+    std::cout << "Pressione ENTER (filename vazio) para finalizar programa.\n";
+
     std::unordered_set<char> alphabet{'a', 'b', 'c'};
     Tokenizer tokenizer{alphabet};
-
-    // std::vector<Token> tokens = tokenizer.tokenize(td.texts[0]);
-    for (const std::string& text : td.texts) { 
-        std::vector<Token> tokens = tokenizer.tokenize(text);
-        std::cout << text << '\n' << tokens << "\n\n";
+    Parser parser{&tokenizer};
+    TextData td{};
+    
+    bool is_running = true;
+    while (is_running) {
+        std::cout << "filename: ";
+        std::string filename;
+        std::getline(std::cin, filename);
+        
+        if (filename.empty()) { is_running = false; continue; }
+        
+        try {
+            td = TextData::load(filename);
+        } catch (const std::runtime_error &err) {
+            std::cout << err.what() << "\n\n";
+            continue;
+        }
+    
+        for (const std::string &text : td.texts) {
+            ParseResult result = parser.parse(text);
+            std::cout << text << ": ";
+            if (result.kind == "VALID") std::cout << "pertence";
+            else std::cout << "nao pertence";
+            std::cout << '\n';
+        }
+        std::cout << '\n';
     }
-
-    std::cout << "> End" << '\n';
+    std::cout << "> Fim: Maquina de Estado Finito" << '\n';
 }
 
-
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector) {
-    os << "[";
-    for (std::size_t i = 0; i < vector.size(); ++i) {
-        os <<  vector[i];
-        if (i < vector.size() - 1) os << ", ";
-    }
-    return os << "]";
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &vector) {
+  os << "[";
+  for (std::size_t i = 0; i < vector.size(); ++i) {
+    os << vector[i];
+    if (i < vector.size() - 1)
+      os << ", ";
+  }
+  return os << "]";
 }
